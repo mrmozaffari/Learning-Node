0fb414271db07361046eab73268f9bfeb044eecf
mrmozaffari <ba.mozaffari@gmail.com>
2020-04-10 00:09:00 -0700

    sd

--- Java/notes-app/app.js
+++ Java/notes-app/app.js
@@ -1,2 +1,11 @@
+const fs = require('fs')
+fs.writeFileSync('notes.txt','Hello Im in the file')
+try{
+  fs.appendFileSync('notes.txt','This is the 2nd')
+  console.log('File appended')
+} catch (err) {
+    console.log('Error')
+}
+
 require('./utils.js')
 console.log('app.js is running')
