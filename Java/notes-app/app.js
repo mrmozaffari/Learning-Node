d60f02ccfad8432e659a76c8238da7ca8e3c8b31
mrmozaffari <ba.mozaffari@gmail.com>
2020-04-09 17:34:36 -0700

    j

--- Java/notes-app/app.js
+++ Java/notes-app/app.js
@@ -1,2 +1,8 @@
 const fs = require('fs')
 fs.writeFileSync('notes.txt','Hello Im in the file')
+try{
+  fs.appendFileSync('notes.txt','This is the 2nd')
+  console.log('File appended')
+} catc (err) {
+  console.log('Error')
+}
