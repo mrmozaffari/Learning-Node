17b9341dd0de15ab8d60637c1e8f401fdcfd150a
mrmozaffari <ba.mozaffari@gmail.com>
2020-04-11 16:55:37 -0700

    fed

--- Java/notes-app/json.js
+++ Java/notes-app/json.js
@@ -2,6 +2,8 @@ const book = {
   'title':'Ego is the Enemy',
   'author': 'Ryan Holiday'
 }
-
+// Print object from json
+console.log(book.title)
+// Convert JSON to String
 const bookJson = JSON.stringify(book)
 console.log(bookJson)
