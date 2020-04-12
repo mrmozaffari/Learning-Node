694887bf347d303341dbad6fd45c04ac9236b7e1
mrmozaffari <ba.mozaffari@gmail.com>
2020-04-11 17:32:05 -0700

    dws

--- Java/notes-app/notes.js
+++ Java/notes-app/notes.js
@@ -1,14 +1,27 @@
-const getNotes = function(){
+const fs = require('fs')
+const getNotes = function() {
   return "These are my notes"
 }
 
-const addNotes = function(title, body){
+const addNotes = function(title, body) {
+  const notes = loadNotes()
+  console.log(notes)
 
+}
+
+const loadNotes = function() {
+  try {
+    const dataBuffer = fs.readFileSync('notes.json')
+    const dataJSON = dataBuffer.toString()
+    return JSON.parse(dataJSON)
+  } catch (e) {
+    return []
+  }
 }
 
 //module.exports = getNotes
 
-module.exports= {
+module.exports = {
   getNotes: getNotes,
   addNotes: addNotes
 }
