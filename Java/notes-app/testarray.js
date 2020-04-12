596df39dcb912db0bf73392406c687faed89102a
mrmozaffari <ba.mozaffari@gmail.com>
2020-04-11 18:58:02 -0700

    dc

--- Java/notes-app/testarray.js
+++ Java/notes-app/testarray.js
@@ -2,12 +2,12 @@ const book = {
   'title':'Ego is the Enemy',
   'author': 'Ryan Holiday'
 }
-console.log('book is ',  book)
 console.log('type of book is ', typeof book)
+console.log('book is ',  book)
 console.log('type of book.title is ', typeof book.title)
 console.log('book.title is ', book.title)
 // Convert JSON to String
 const bookJsonString = JSON.stringify(book)
 
-console.log('bookJsonString is ', bookJsonString)
 console.log('type of bookJsonString is ', typeof bookJsonString)
+console.log('bookJsonString is ', bookJsonString)
