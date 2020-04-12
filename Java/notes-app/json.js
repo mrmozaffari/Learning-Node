9c46d65ea9c74ee20690efa20d4ec3c5d5916e2f
mrmozaffari <ba.mozaffari@gmail.com>
2020-04-11 17:02:00 -0700

    d

--- Java/notes-app/json.js
+++ Java/notes-app/json.js
@@ -1,3 +1,4 @@
+const fs = require('fs')
 const book = {
   'title':'Ego is the Enemy',
   'author': 'Ryan Holiday'
@@ -8,6 +9,8 @@ console.log(book.title)
 // Convert JSON to String
 const bookJsonString = JSON.stringify(book)
 console.log(bookJsonString)
+fs.writeFileSync('myjson.json',bookJsonString) //save json string to file
+
 
 //Convert string to json
 const bookJson = JSON.parse(bookJsonString)
