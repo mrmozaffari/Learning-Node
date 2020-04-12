e48dcde33054f78637f0218df5cfeeddec2120e4
mrmozaffari <ba.mozaffari@gmail.com>
2020-04-11 19:00:29 -0700

    sd

--- Java/notes-app/testarray.js
+++ Java/notes-app/testarray.js
@@ -6,9 +6,14 @@ console.log('type of book is ', typeof book)
 console.log('book is ',  book)
 console.log('type of book.title is ', typeof book.title)
 console.log('book.title is ', book.title)
-// Convert JSON to String
-const bookJsonString = JSON.stringify(book)
 
-console.log('type of bookJsonString is ', typeof bookJsonString)
-console.log('bookJsonString is ', bookJsonString)
-console.log('bookJsonString is ', bookJsonString.title)
+// Convert JSON to String
+const bookString = JSON.stringify(book)
+console.log('type of bookString is ', typeof bookString)
+console.log('bookString is ', bookString)
+
+// Convert String to json
+
+const bookJson = JSON.parse(bookString)
+console.log('type of bookJson is ', typeof bookJson)
+console.log('bookJson is ', bookJson)
