53c7874a59a04e26d3595fcecda206c7db02b3f0
mrmozaffari <ba.mozaffari@gmail.com>
2020-04-13 01:12:04 -0700

    df

--- Java/notes-app/notes.js
+++ Java/notes-app/notes.js
@@ -6,7 +6,13 @@ const getNotes = function() {
 
 const delNotes = function(title){
   const notes = loadNotes()
+  if(ifExist == true){
+    console.log('Hey we have a match in our data')
+  }
   console.log(notes)
+  const ifExist = notes.filter(function(note){
+    return note.title === title
+  })
 }
 
 
