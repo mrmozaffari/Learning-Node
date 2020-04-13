d42002d891af4363d61a9a780b8806e960005440
mrmozaffari <ba.mozaffari@gmail.com>
2020-04-12 21:09:46 -0700

    ds

--- Java/notes-app/testarray2.js
+++ Java/notes-app/testarray2.js
@@ -1,7 +1,7 @@
 //To convert object to string
 var data = [{id: 1, name:'personsName'}, {id: 2, name:'personsName2'}]
-//var string = JSON.stringify(data)
-var object = JSON.parse(data)
+var string = JSON.stringify(data)
+var object = JSON.parse(string)
 console.log(string)
 console.log(string.id)
 console.log(data.id)
