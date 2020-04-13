cc36cb9dbcf1be89b6c0c0d44162bd22bc769265
mrmozaffari <ba.mozaffari@gmail.com>
2020-04-12 21:20:01 -0700

    ef

--- Java/notes-app/testarray2.js
+++ Java/notes-app/testarray2.js
@@ -1,10 +1,9 @@
 const yargs = require('yargs')
-
 //To convert object to string
 
-var data = [{id: 1, name:'personsName'}, {id: 2, name:'personsName2'}]
-var string = JSON.stringify(data)
-var object = JSON.parse(string)
+//var data = [{id: 1, name:'personsName'}, {id: 2, name:'personsName2'}]
+//var string = JSON.stringify(data)
+//var object = JSON.parse(string)
 
 
 yargs.command({
